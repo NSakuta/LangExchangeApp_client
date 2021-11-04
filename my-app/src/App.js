f75e68e94d09f9a309c6edca8724fe5cec4cf177
NSakuta <natalie.kutnaya@gmail.com>
2021-11-04 18:57:12 +0100

    Header component is added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import User from './components/User';
+import Header from './components/Header/Header';
+import {Route, Switch} from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div>
+      <Header/>
       <User></User>
     </div>
   );
