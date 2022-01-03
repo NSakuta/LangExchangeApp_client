77ae0dc937d4764ddb80a72ab9bddc4f1aeb4c10
NSakuta <natalie.kutnaya@gmail.com>
2022-01-03 18:03:55 +0100

    build file changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+const https = require('https');
+
+
 
 const client = axios.create({
-    baseURL: 'http://ec2-3-21-19-104.us-east-2.compute.amazonaws.com/api'
+    baseURL: 'http://ec2-3-21-19-104.us-east-2.compute.amazonaws.com/api',
+    httpsAgent: new https.Agent({  
+        rejectUnauthorized: false
+      })
 })
 
 export default client;
